bd19675052c8196591f99a74e4c1b473d00f508d
krishgoyal0 <kshricastav@gmail.com>
2026-05-22 00:40:57 +0530

    .

---
+++ electrode-qc-lab/src/main/java/com/qc/electrode_qc_lab/ElectrodeQcLabApplication.java
@@ -0,0 +1,13 @@
+package com.qc.electrode_qc_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrodeQcLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrodeQcLabApplication.class, args);
+	}
+
+}
---
+++ electrode-qc-lab/src/test/java/com/qc/electrode_qc_lab/ElectrodeQcLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qc.electrode_qc_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrodeQcLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
